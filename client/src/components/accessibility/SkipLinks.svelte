73dabf103e66b5181435c91235344efb44abce56
distantorigin <distantorigin@distantorig.in>
2026-02-21 21:43:14 -0600

    fix: reduce skip links to only skip to input

--- client/src/components/accessibility/SkipLinks.svelte
+++ client/src/components/accessibility/SkipLinks.svelte
@@ -1,9 +1,3 @@
-<script lang="ts">
-  // Skip links provide keyboard users quick navigation to main content areas.
-</script>
-
 <nav aria-label="Skip links">
-  <a class="skip-link" href="#output-region">Skip to output</a>
-  <a class="skip-link" href="#command-input">Skip to command input</a>
-  <a class="skip-link" href="#user-list">Skip to user list</a>
+  <a class="skip-link" href="#command-input">Skip to input</a>
 </nav>
